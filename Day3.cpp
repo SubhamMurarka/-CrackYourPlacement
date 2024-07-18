2a7719c0a958431c61bdad37487768bd53bafab1
subham murarka <shubham.murarkag@gmail.com>
2024-07-18 18:21:41 +0530

    DAY3

---
+++ Day3.cpp
@@ -0,0 +1,52 @@
+//Q7 : longest common prefix
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string chkStr = strs[0];
+        int ind = chkStr.size()-1; //5
+        string ans = "";
+        int k = -1;
+        for(int i=1; i<strs.size(); i++) {
+            string s = strs[i];
+            for(int j=0; j<s.size(); j++) {
+                if(j <= ind && chkStr[j] == s[j]) k = j; 
+                else break;
+            }
+            ind = k;
+            k = -1;
+        }
+        if(ind != -1) {
+            for(int i=0; i<=ind; i++) ans += chkStr[i];
+        }
+        return ans;
+    }
+};
+
+
+//Q8 : Grouping Anagrams // to check time and space complexity
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<string> str = strs;
+        unordered_map<string, vector<string>> mp;
+        
+        for(int i=0; i<str.size(); i++) {
+            sort(str[i].begin(), str[i].end());
+            mp[str[i]].push_back(strs[i]);
+        }
+        
+        // s(eat) -- eat, tea, ate
+        
+        vector<vector<string>>ans;
+        
+        for(auto it: mp) {
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+    }
+};
+
+//Q9 : 
